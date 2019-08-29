4b4314f83e92aa59a82b04f0452796f7a4dd03fa
jsikka <jsikka@adobe.com>
2019-08-30 01:25:01 +0530

    Uploaded dataset and features

---
+++ CodeDataset/1174.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    //A题
+    public static void main(String[] args) {
+        Scanner mych = new Scanner(System.in);
+        int n = mych.nextInt();
+        //M要为偶数，E要为奇数
+        //从小到大列举，并且n要减去a
+        //如果n是奇数
+        //如果M要赢，则M面临的
+        //如果n是偶数，一定M
+        if(n % 2== 0){
+            System.out.println("Mahmoud");
+        }
+        else{//如果是奇数
+            System.out.println("Ehab");
+        }
+    }
+}
+
+ 	  	   	       		  	  	  	
